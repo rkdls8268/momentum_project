27f4428eef4c7e470c0ca83da85fd16d69f363de
rkdls8268 <rkdls8268@gmail.com>
2020-11-09 15:57:03 +0900

    [실습] currentPosition save&load

---
+++ weather.js
@@ -0,0 +1,40 @@
+const API_KEY = "c1c00ef79cbffea261e6508335fa8b4d";
+
+const COORDS = 'coords';
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+    console.log("can't find geo location")
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords === null) {
+        askForCoords();
+    } else {
+        // getWeather
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
